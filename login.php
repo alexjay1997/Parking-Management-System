b4be56b9795c323afc0a2301ceeeefaa610b2eb3
alexjay1997 <aljayfauni@gmail.com>
2020-12-09 04:00:27 +0800

    update db connetion for heroku

--- login.php
+++ login.php
@@ -3,6 +3,57 @@
 //$conn_total_slots = new Select_class();
 //$read_all_numrows_total_slots= $conn_select_all_entry->select_all_slots();
 //$total_slots = mysqli_fetch_array($read_all_numrows_total_slots);
+
+if(isset($_POST['btn_login'])){
+
+  
+    session_start();
+    
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+     
+     $server = $url["host"];
+     $dbusername = $url["user"];
+     $dbpassword = $url["pass"];
+     $db = substr($url["path"], 1);
+     
+     $conn = new mysqli($server, $dbusername, $dbpassword, $db);
+    
+    $username =mysqli_real_escape_string($conn_select_user_login->connection,$_POST['username']);
+    $password = mysqli_real_escape_string($conn_select_user_login->connection,$_POST['password']);
+    $Encrypt_password = $password;
+    
+     $query=mysqli_query($conn,"select * from tbl_users where username='$username' && password='$Encrypt_password'");
+    
+    if($query->num_rows>0){
+      $row =mysqli_fetch_array($query);
+        
+        //if($row['role']=="admin"){
+       // if (isset($_POST['remember'])){
+            //set up cookie
+        //    setcookie("user", $row['username'], time() + (86400 * 30)); 
+       //     setcookie("pass", $row['password'], time() + (86400 * 30)); 
+       // }
+    
+        $_SESSION['id']=$row['id'] ;
+        header('location:admin_panel.php');
+       //}
+    
+       //else if($row['role']=="staff"){
+    
+    
+        
+       // $_SESSION['id_staff']=$row['id'];
+    
+       // header('location:../index.php');
+       //}
+    }
+    else{
+    
+      header('location:../login.php');
+    }
+    
+    }
+
 ?>
 
 
