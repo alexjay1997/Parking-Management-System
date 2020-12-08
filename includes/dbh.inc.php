82ad13eae5555fcbe1fdfe73496e7e657b0ba6aa
alexjay1997 <aljayfauni@gmail.com>
2020-12-09 03:03:10 +0800

    update db connetion for heroku

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -12,22 +12,22 @@
 <?php
  class Database{
  
-    public $db_host;
-    public $db_username;
-    public $db_password;
-    public $db_name;
+    public $server;
+    public $username;
+    public $password;
+    public $db;
   
     public function db_connection()
     {
        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         
-       $db_host =$url["host"];
-        $db_username =$url["user"];
-       $db_password ="pass";
-        $db_name =substr($url["path"],1);
+       $server =$url["host"];
+        $username =$url["user"];
+       $password ="pass";
+        $db =substr($url["path"],1);
 
      
-        $connection = new mysqli($db_host,$db_username,$db_password,$db_name);
+        $connection = new mysqli($server,$username,$password,$db);
         return $connection;
     }
 
