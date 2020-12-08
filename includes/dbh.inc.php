a2a0bc65247bc599746b06c893cc28263b13ab70
alexjay1997 <aljayfauni@gmail.com>
2020-12-08 21:24:28 +0800

    change db connection for heroku mysql

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -8,10 +8,10 @@
 
     protected function db_connection()
     {
-        $this->db_host ="localhost";
-        $this->db_username ="root";
-        $this->db_password ="";
-        $this->db_name ="db_pms";
+        $this->db_host ="us-cdbr-east-02.cleard";
+        $this->db_username ="b749a6c04fe595";
+        $this->db_password ="1e7cf907";
+        $this->db_name ="heroku_f4bacc0811464cd";
 
         $this->connection = new mysqli($this->db_host,$this->db_username,$this->db_password,$this->db_name);
         return $this->connection;
