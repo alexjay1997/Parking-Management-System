de61fe81c857b35e7ad0159f71f8b5f315b078f4
alexjay1997 <aljayfauni@gmail.com>
2020-12-08 22:13:02 +0800

    change db connection2 to clearDB  for heroku

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -24,7 +24,7 @@
         $this->db_host =$url["us-cdbr-east-02.cleard"];
         $this->db_username =$url["b749a6c04fe595"];
         $this->db_password =$url["1e7cf907"];
-        $this->db_name =substr($url["heroku_f4bacc0811464cd"],1);
+        $this->db_name =substr($url["heroku_f4bacc0811464cd"]);
 
         $this->connection = new mysqli($this->db_host,$this->db_username,$this->db_password,$this->db_name);
         return $this->connection;
