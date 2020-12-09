39094ae4d5899eed9fe6c11cf317c46d12857919
alexjay1997 <aljayfauni@gmail.com>
2020-12-09 16:01:15 +0800

    fix connection db

--- functions/login.func.php
+++ functions/login.func.php
@@ -1,6 +1,6 @@
 <?php
- // include '../includes/Select.class.php';
- // $conn_select_user_login =new Select_class();
+ include '../includes/Select.class.php';
+  $conn_select_user_login =new Select_class();
  //<?php
  
  
@@ -15,43 +15,44 @@ if(isset($_POST['btn_login'])){
   
 session_start();
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
  
- $server = $url["host"];
- $dbusername = $url["user"];
- $dbpassword = $url["pass"];
- $db = substr($url["path"], 1);
+ //$server = $url["host"];
+ //$dbusername = $url["user"];
+ //$dbpassword = $url["pass"];
+ //$db = substr($url["path"], 1);
  
- $conn = new mysqli($server, $dbusername, $dbpassword, $db);
+ //$conn = new mysqli($server, $dbusername, $dbpassword, $db);
 
 $username =mysqli_real_escape_string($conn_select_user_login->connection,$_POST['username']);
 $password = mysqli_real_escape_string($conn_select_user_login->connection,$_POST['password']);
 $Encrypt_password = $password;
 
- $query=mysqli_query($conn,"select * from tbl_users where username='$username' && password='$Encrypt_password'");
+$select_login_user = $conn_select_user_login->select_login_user($username,$Encrypt_password);
+ //$query=mysqli_query($conn,"select * from tbl_users where username='$username' && password='$Encrypt_password'");
 
-if($query->num_rows>0){
-  $row =mysqli_fetch_array($query);
+if($select_login_user->num_rows>0){
+  $row =mysqli_fetch_array($select_login_user);
     
-    //if($row['role']=="admin"){
+    if($row['role']=="admin"){
    // if (isset($_POST['remember'])){
         //set up cookie
     //    setcookie("user", $row['username'], time() + (86400 * 30)); 
    //     setcookie("pass", $row['password'], time() + (86400 * 30)); 
    // }
 
-    $_SESSION['id']=$row['id'] ;
+    $_SESSION['id_admin']=$row['id'] ;
     header('location:../admin_panel.php');
-   //}
+   }
 
-   //else if($row['role']=="staff"){
+   else if($row['role']=="staff"){
 
 
     
-   // $_SESSION['id_staff']=$row['id'];
+    $_SESSION['id_staff']=$row['id'];
 
-   // header('location:../index.php');
-   //}
+    header('location:../index.php');
+   }
 }
 else{
 
