31d89202df7787b303625b70ee2bd051d3af4aa1
alexjay1997 <aljayfauni@gmail.com>
2020-12-09 17:11:26 +0800

    update function update

--- functions/update_available_slot.php
+++ functions/update_available_slot.php
@@ -9,9 +9,9 @@ if(isset($_POST['btn_update_available_slot'])){
 $available_slots =mysqli_real_escape_string($conn_update_parking_slots->connection,$_POST['inp_update_available_slot']);
 $update_available_slot = $conn_update_parking_slots->update_available_slots($available_slots);
 
-if($update_available_slot){
+//if($update_available_slot){
 header('location:../index.php');
-}
+//}
 }
 
 
--- functions/update_parking_slot.php
+++ functions/update_parking_slot.php
@@ -9,14 +9,14 @@ if(isset($_POST['btn_update_parking_slot'])){
 $total_slots = mysqli_real_escape_string($conn_update_parking_slots->connection,$_POST['inp_update_parking_slot']);
 $update_total_parking_slot = $conn_update_parking_slots->update_parking_slots($total_slots);
 
-if($update_total_parking_slot){
+//if($update_total_parking_slot){
 header('location:../admin_panel.php');
-}
+//}
 
-else{
-    header('location:../admin_panel.php');
+//else{
+ //   header('location:../admin_panel.php');
 
-}
+//}
 
 
 }
