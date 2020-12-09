dc445dbae937c21e1dd84ad3fc523f14f9edcff5
alexjay1997 <aljayfauni@gmail.com>
2020-12-09 21:50:29 +0800

    update function update

--- parking.panel.php
+++ parking.panel.php
@@ -109,7 +109,7 @@ $row = mysqli_fetch_assoc($read_login_current);
                     <div class="row d-flex justify-content-center">
                    
                         <!--form -->
-                        <form method="GET" action="functions/add.func.entry.php" class="text-center">
+                        <form method="POSt" action="function/add.func.entry.php" class="text-center">
                      
 
 
