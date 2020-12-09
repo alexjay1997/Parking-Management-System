6c72eb834f196f6b2be480cd533255a53851babd
alexjay1997 <aljayfauni@gmail.com>
2020-12-09 17:42:07 +0800

    update function update

--- includes/Insert.class.php
+++ includes/Insert.class.php
@@ -9,7 +9,7 @@ class Insert_class extends Database{
     }
     public function insert_parking_entry($vehicle, $price){
 
-        $query_insert_parking_entry="INSERT INTO `tbl_parking_entry`(vehicle, price) values ('$vehicle','$price')";
+        $query_insert_parking_entry="INSERT INTO `tbl_parking_entry`(vehicle, price) values ('.$vehicle.','.$price.')";
         $result=mysqli_query($this->connection,$query_insert_parking_entry);
         return $result;
 
@@ -17,7 +17,7 @@ class Insert_class extends Database{
 
     public function update_parking_slots($total_slots){
 
-        $query_update_parking_slots="UPDATE `tbl_parking_slots` SET total_parking_slots='$total_slots' WHERE id=1 ";
+        $query_update_parking_slots = "UPDATE `tbl_parking_slots` SET total_parking_slots ='.$total_slots.' WHERE id=1 ";
         $result=mysqli_query($this->connection,$query_update_parking_slots);
         return $result;
 
@@ -25,7 +25,7 @@ class Insert_class extends Database{
 
     public function update_available_slots($available_slots){
 
-        $query_update_available_slots="UPDATE `tbl_parking_slots` SET available_slots='$available_slots' WHERE id=1 ";
+        $query_update_available_slots="UPDATE `tbl_parking_slots` SET available_slots = '.$available_slots.' WHERE id=1 ";
         $result=mysqli_query($this->connection,$query_update_available_slots);
         return $result;
 
